53dcb585430d618a40ff4e9e9ef4bf22022ca280
amardeeps <sarkar.amardeep01@gmail.com>
2024-03-05 23:10:26 +0530

    add useEffect and dispatch in App.jsx

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import config from './config/config'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/authService'
+import {login, logout} from './store/authSlice'
+
 
 function App() {
 
-  // const conf = config
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  // console.log(conf.appwriteProjectId)
-  console.log(config.appwriteCollectionId)
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        // test by changing the userData variable here and in the slice page
+        dispatch(() => login({userData}))
+        
+      } else {
+        dispatch(() => logout())
+      }
+    })
+    .finally(() => setLoading(false))
+
+  }, [])
+
+
 
   return (
     <>
