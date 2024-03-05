9c96b14e322a5c090fde84e6b4d0378b15a47845
amardeeps <sarkar.amardeep01@gmail.com>
2024-03-05 23:56:32 +0530

    add componet and create main layout, dispatch problem is still there

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -4,35 +4,45 @@ import config from './config/config'
 import {useDispatch} from 'react-redux'
 import authService from './appwrite/authService'
 import {login, logout} from './store/authSlice'
+import { Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 
 function App() {
 
   const [loading, setLoading] = useState(true)
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-  // useEffect(() => {
-  //   authService.getCurrentUser()
-  //   .then((userData) => {
-  //     if (userData) {
-  //       // test by changing the userData variable here and in the slice page
-  //       dispatch(() => login({userData}))
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        // test by changing the userData variable here and in the slice page
+        dispatch(() => login({userData}))
+        
+      } else {
+        dispatch(() => logout())
+      }
+    })
+    .finally(() => setLoading(false))
 
-  //     } else {
-  //       dispatch(() => logout())
-  //     }
-  //   })
-  //   .finally(() => setLoading(false))
-
-  // }, [])
+  }, [])
 
 
 
-  return (
-    <>
-        <h1 style={{color: "white"}}>Blog With AppWrite</h1>        
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap
+    content-between bg-gray-200'>
+      <div className='w-full block'>
+          <Header />
+          <main>
+            {/* <Outlet /> */}
+          </main>
+          <Footer />
+      </div>
+
+    </div>
+  ) : null;
 }
 
 export default App
