e2fa8f81f9ba635be9486252db6e35be44cff903
amardeeps <sarkar.amardeep01@gmail.com>
2024-03-05 23:46:22 +0530

    comment dispatch code due to error

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -9,22 +9,22 @@ import {login, logout} from './store/authSlice'
 function App() {
 
   const [loading, setLoading] = useState(true)
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-  useEffect(() => {
-    authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        // test by changing the userData variable here and in the slice page
-        dispatch(() => login({userData}))
-        
-      } else {
-        dispatch(() => logout())
-      }
-    })
-    .finally(() => setLoading(false))
+  // useEffect(() => {
+  //   authService.getCurrentUser()
+  //   .then((userData) => {
+  //     if (userData) {
+  //       // test by changing the userData variable here and in the slice page
+  //       dispatch(() => login({userData}))
 
-  }, [])
+  //     } else {
+  //       dispatch(() => logout())
+  //     }
+  //   })
+  //   .finally(() => setLoading(false))
+
+  // }, [])
 
 
 
