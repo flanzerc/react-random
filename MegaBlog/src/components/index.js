1a8b703e44241abb7f2347ba9aee34310dd07bb0
amardeeps <sarkar.amardeep01@gmail.com>
2024-03-14 23:41:06 +0530

    PostForm component functionality

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,11 +3,20 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import RTE from "./RTE";
+import Select from "./Select"
+import Input from "./Input"
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    RTE,
+    Select,
+    Input,
+
 }
